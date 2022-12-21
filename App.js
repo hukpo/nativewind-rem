8ab89ffb2a4c30db450898688d2b317d078a7884
Pavlo Huk <pavlo.huk@icloud.com>
2022-12-21 14:26:08 +0200

    add min-h bug view

--- App.js
+++ App.js
@@ -6,7 +6,11 @@ NativeWindStyleSheet.setVariables({ "--rem": "10px" });
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <View className="w-remTest aspect-square bg-[#ffdd00]" />
+      {/* works */}
+      <View className="w-remTest min-h-[40px] bg-[#ffdd00]" />
+
+      {/* bug */}
+      <View className="w-remTest min-h-remTest bg-[#ddff00]" />
     </View>
   );
 }
